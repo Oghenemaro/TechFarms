1e06723dcddc96f55976172116151c0d31848db7
Maro <marowinse@outlook.com>
2018-08-31 12:10:18 +0100

    Ensure app is running and test in initially failing

---
+++ server/test/test.js
@@ -0,0 +1,58 @@
+import app from '../../app';
+
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+require('chai').should();
+
+chai.use(chaiHttp);
+
+describe('Test Data', () => {
+    let userData, farmData, reviewData;
+    beforeEach(() => {
+        userData = {
+            username: 'marowinsei',
+            firstname: 'maro',
+            lastname: 'george',
+            email: 'marogeorge@gmail.com',
+            cell: '08073349770'
+        };
+        farmData = {
+            farmname: 'samivy',
+            farmtype: 'poultry',
+            productQuantity: '10,000',
+            location: 'Otta Ogun state',
+            size: '1 acre',
+            insuranceStatus: 'insured'
+        };
+        reviewData = {
+            review: 'quality enterprise',
+            like: 'active',
+            dislike: 'deactive',
+            farm: 'samivy',
+            user: 'marowinsei'
+        }
+    });
+
+    describe('User Test', () => {
+        context('handle valid signup', () => {
+            it('should create a new user', () => {
+                chai.request(app)
+                .post('/api/v1/auth/signup')
+                .send(userData)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.an.instanceOf(object);
+                });
+            });
+            it('should authenticate a user', () => {
+                chai.request(app)
+                .post('/api/v1/auth/signup')
+                .send(userData)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.an.instanceOf(object);
+                });
+            });
+        });
+    });
+});
