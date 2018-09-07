0a14e9571ec95485e91c43c307bcd4f9a00fe012
Maro <marowinse@outlook.com>
2018-09-07 16:51:27 +0100

    Create feature to allow users invest in a farm

--- server/controller/farms.js
+++ server/controller/farms.js
@@ -1,16 +1,97 @@
 import db from '../models/index';
 
 const farms = db.farms;
+const product = db.products;
+const userInvestment = db.investments;
 
-class Farms{
-    static viewFarms(req, res){
-        if (farms === undefined){
-            return res.status(400).send({Status: 'failed', message: "Can't load farms, please try again"});
+class Farms {
+    static viewFarms(req, res) {
+        if (farms === undefined) {
+            return res.status(400).send({ Status: 'failed', message: "Can't load farms, please try again" });
         }
         farms.findAll({
-            attributes: ['id', 'farmname', 'farmType', 'size', 'insuranceStatus']
-        }).then((farm) => res.status(200).send({Status: 'successful', Farms: farm}))
-        .catch((err) => res.status(400).send({Status: 'failed', message: 'A problem occured, please try again'}));
+            attributes: ['id', 'farmname', 'productID', 'address', 'size', 'insuranceStatus'],
+        }).then((farm) => {
+            res.status(200).send({ Status: 'successful', Farms: farm });
+        })
+            .catch((err) => res.status(400).send({ Status: 'failed', message: err }));
+    }
+
+    static viewFarm(req, res) {
+        const farmSelected = req.params.id;
+        farms.findOne({
+            include: [product],
+            attributes: ['id', 'farmname', 'productID', 'address', 'size', 'insuranceStatus'],
+            where: {
+                id: farmSelected
+            }
+        }).then((farmFound) => {
+            const { livestock, minimumInvestment, percentageReturn, duration } = farmFound.product;
+            const farmRecord = {
+                farmID: farmFound.id,
+                livestock,
+                minimumInvestment,
+                percentageReturn,
+                duration
+            }
+            console.log(farmRecord);
+            res.status(200).send({ Status: 'successful', Farms: farmRecord });
+        }).catch((err) => res.status(400).send({ Status: 'failed', Farms: 'A problem occured, cannot loadd all farms at this moment' }));
+    }
+
+    static determineReturns(amountPaid, minimumInvestment, percentageROI) {
+        const totalAmount = parseInt(minimumInvestment) + parseInt(amountPaid);
+        if (totalAmount < minimumInvestment) {
+            return 'Investment must be larger than minimum value';
+        }
+        const percentageEarnings = totalAmount * (parseInt(percentageROI) / 100);
+        const totalEarnings = totalAmount + percentageEarnings;
+        return totalEarnings;
+    }
+
+    //     static findFarm(farmSelected) {
+    //         return farms.findOne({
+    //             // include: [product],
+    //             attributes: ['id', 'farmname', 'productID', 'address', 'size', 'insuranceStatus'],
+    //             where: {
+    //                 id: 2
+    //             }
+    //         });
+    // }
+
+    static createInvestment(req, res) {
+        const farmID = req.params.id;
+        const { investment } = req.body;
+        if (farmID === undefined) {
+            return res.status(400).send({ Status: 'failed', message: 'farm not selected' });
+        }
+        if (investment === undefined) {
+            return res.status(400).send({ Status: 'failed', message: 'Enter an investment value' });
+        }
+        farms.findOne({
+            include: [product],
+            attributes: ['id', 'farmname', 'productID', 'address', 'size', 'insuranceStatus'],
+            where: {
+                id: farmID
+            }
+        }).then((farmFound) => {
+            const { livestock, minimumInvestment, percentageReturn, duration } = farmFound.product;
+            if (livestock && minimumInvestment && percentageReturn && duration === undefined){
+                return res.status(400).send({ Status: 'failed', message: 'farm not found' });
+            }
+            const totalEarnings = Farms.determineReturns(investment, minimumInvestment, percentageReturn);
+            if (totalEarnings === undefined) {
+                return res.status(400).send({ Status: 'failed', message: 'earnings not determined' });
+            }
+            return userInvestment.create({
+                userID: req.body.userID,
+                farmID: farmFound.id, 
+                amountPaid: investment,
+                ROI: percentageReturn,
+                totalEarnings
+            }).then(() => res.status(200).send({ Status: 'successful', mesage: 'Your investment is complete, happy earnings!' }))
+                .catch((err) => res.status(400).send({ Status: 'failed', message: err }));
+        })
     }
 }
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -13,5 +13,7 @@ router.get('/api', (req, res) => res.status(200).send({
 router.post('/api/v1/auth/signup',  CheckInputs.checkCreateInputs, Users.createUser);
 router.post('/api/v1/auth/signin', CheckInputs.checkLoginInputs, Users.login);
 router.get('/api/v1/farms', Farms.viewFarms);
+router.get('/api/v1/farm/:id', Farms.viewFarm);
+router.post('/api/v1/farms/:id', LoginStatus.locateToken, LoginStatus.verifyToken, Farms.createInvestment);
 
 module.exports = router;
